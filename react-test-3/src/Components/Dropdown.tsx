0cacd0095573581160b6f26a7e10d94d165349de
yukuan95 <zhaoyukuan95@gmail.com>
2026-06-24 21:54:57 +0900

    .

--- react-test-3/src/Components/Dropdown.tsx
+++ react-test-3/src/Components/Dropdown.tsx
@@ -1,10 +1,10 @@
-import { Dropdown, Button, ConfigProvider, theme } from 'antd'
+import { Dropdown as AntdDropdown, Button, ConfigProvider, theme } from 'antd'
 import { DownOutlined } from '@ant-design/icons'
 import { css } from '@emotion/css'
 
 type Props = { value: string; array: Array<string>; width: string; isDarkMode: boolean; onChange: (res: string) => void }
 
-function Dropdown2({ value, array, width, isDarkMode, onChange }: Props) {
+function Dropdown({ value, array, width, isDarkMode, onChange }: Props) {
   const items = array.map((item: string) => ({ label: item, key: item }))
   const menuProps = {
     items, onClick: (res: any) => onChange(res.key),
@@ -22,15 +22,15 @@ function Dropdown2({ value, array, width, isDarkMode, onChange }: Props) {
           },
         },
       }}>
-      <Dropdown menu={menuProps} trigger={['click']}>
+      <AntdDropdown menu={menuProps} trigger={['click']}>
         <Button className={css` width: ${width}; justify-content: space-between; `}
           icon={<DownOutlined />} iconPlacement="end">
           {array.find((item) => item === value) ? value : ''}
         </Button>
-      </Dropdown>
+      </AntdDropdown>
     </ConfigProvider>
   </div >
 }
 
 
-export { Dropdown2 as Dropdown }
+export { Dropdown }
